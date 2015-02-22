3f3f17e9cb004392bf248847a4cab6f9c9be3b53
Ewan Gatherer <ewangatherer@gmail.com>
2015-02-21 23:50:21 -0600

    Updated comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,20 +1,25 @@
 ## makeCacheMatrix provides 4 functions, set, get setmatrix and getmatrix
-## providing the capability to cache a matrix
+## providing the capability to create & cache a matrix
 ## cacheSolve inverts a matrix, leveraging the potentially cached matrix
 ## cacheSolve will store the inverted matrix for later retrieval on the first execution
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    #2 variables are maintained, x as the new matrix, mat as the cached matrix
+    
     mat <- NULL
+    #set takes the input, applies it to the orginating matrix and overwrites any cached matrix
     set <- function(y) {
         x <<- y
         mat <- NULL
     }
-    
+    #simple return of the existing matrix by means of a function
     get <- function() x
     
+    #sets the cached value to that of the variable inverse by means of a function
     setmatrix <- function(inverse) mat <<- inverse
     
+    #returns a function that returns the value of the cached matrix
     getmatrix <- function() mat
     list (set = set, get = get, setmatrix = setmatrix, getmatrix = getmatrix)
     
@@ -36,3 +41,4 @@ cacheSolve <- function(x, ...) {
     x$setmatrix(mat)
     mat
 }
+
